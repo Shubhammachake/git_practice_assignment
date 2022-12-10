98fd2cc2467c3fc83afb3b0693ccf668ccdfd4fa
Shubham machake <machakeshubh@gmail.com>
2022-12-10 16:20:02 +0530

    done

--- prime.js
+++ prime.js
@@ -1,10 +1,10 @@
 let num=15; 
 let count=0;
-    for(i=i;i<=num;i++){
+    for(i=1;i<=num;i++){
         if(num%i==0 && num%num==0){
             count++;
         }
-    }if(count==1){
+    }if(count==2){
         console.log("Its a Prime Number");
     }else{
         console.log("Its Not a Prime Number");
